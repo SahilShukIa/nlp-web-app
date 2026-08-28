c1674c5b53f7eceee52949df5409b9d1e690786d
SahilShukIa <real.sahilshukla@gmail.com>
2026-08-29 00:40:49 +0530

    Added login and registration page and also linked each other as well

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/login')
 def index():
-    return "Hello World"
+    return render_template('login.html')
 
-app.run()
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+app.run(debug=True) # debug=True prevents from running again and again 
